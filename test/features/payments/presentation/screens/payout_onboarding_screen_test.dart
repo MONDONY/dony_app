d16fabeed018ae58128d0a63deecf05ebb18b37c
Aboubakar Diakite <57750832+aboubakardiakite@users.noreply.github.com>
2026-09-24 23:34:08 +0200

    style: ordre des imports du test de versement

--- test/features/payments/presentation/screens/payout_onboarding_screen_test.dart
+++ test/features/payments/presentation/screens/payout_onboarding_screen_test.dart
@@ -18,11 +18,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mocktail/mocktail.dart';
 
+import '../../../../helpers/l10n_test_helpers.dart';
 // `MockStripeAccountBloc` est déjà déclaré localement ci-dessous : on n'importe
 // que la constante d'état pour éviter la collision de noms.
 import '../../../../helpers/stripe_account_test_doubles.dart'
     show stripeCountryUnavailableState;
-import '../../../../helpers/l10n_test_helpers.dart';
 
 class MockPaymentBloc extends MockBloc<PaymentEvent, PaymentState>
     implements PaymentBloc {}
